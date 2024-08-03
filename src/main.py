8d98e9231786aad74b333667411c6b6a66430874
Marius-Juston <Marius.juston@hotmail.fr>
2024-08-03 15:26:04 -0400

    added exmaple slash command

--- src/main.py
+++ src/main.py
@@ -1,13 +1,38 @@
+from typing import Any, Optional, Literal
+
 import discord
+from discord import app_commands, Intents
 from dotenv import dotenv_values
 
 
 class MyClient(discord.Client):
+    def __init__(self, *, intents: Intents, **options: Any):
+        super().__init__(intents=intents, **options)
+
+        self.synced = False
+
+        self.tree = app_commands.CommandTree(self)
+
+        self.running = self.tree.command(name='running', description="running faster")(self.running)
+
     async def on_ready(self):
         print(f'Logged on as {self.user}!')
 
+        print(self.tree)
+
+        await self.wait_until_ready()
+        if not self.synced:
+            print("Syncing tree")
+            await self.tree.sync()
+            print("Synced tree")
+            self.synced = True
+
     async def on_message(self, message):
         print(f'Message from {message.author}: {message.content}')
+
+    async def running(self, interaction: discord.Interaction, name: str, infor: int,
+                      superman: Optional[Literal['help', 'cheese']]):
+        print("Hello", interaction, name)
 
 
 if __name__ == '__main__':
