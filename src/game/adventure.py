a226246f60d715c34125efe3aa95be33a5a9d3fc
ArmandJ1 <aretherebouts@proton.me>
2024-08-03 16:42:48 -0400

    Start of /start_adventure

--- src/game/adventure.py
+++ src/game/adventure.py
@@ -1,5 +1,5 @@
 import discord
-
+user_happy = False
 
 class Adventure:
     def __init__(self, theme, lore):
@@ -8,19 +8,31 @@ class Adventure:
         self.theme = theme
         self.lore = lore
 
+    async def adventure_start(self,ctx):
+        #TODO :  add the player list 
+        embed=discord.Embed(title="New Adventure", description="This is an embed that will show how to build an embed and the different components", color=0x109319)
+        embed.add_field(name="Theme", value=self.theme, inline=False)
+        embed.add_field(name="Lore", value=self.lore, inline=True)
+        embed.add_field(name="Player list", value=[], inline=True)
+        embed.set_footer(text="Please use /Join_Adventure to join current adventure and type /begin adventure")
+
     def generate_lore(self):
-        # FUTURE WILL BE DONE USING LLM
+        # TODO : FUTURE WILL BE DONE USING LLM
 
         return "The pie is a lie"
 
-    def process_lore(self, interaction: discord.Interaction):
-        # GENERATE LORE HERE IF IT DOES NOT EXIST AND ASK USER IF THE CURRENT LORE IS GOOD OR NOT
+    async def process_lore(self, interaction: discord.Interaction):
+        # TODO : GENERATE LORE HERE IF IT DOES NOT EXIST AND ASK USER IF THE CURRENT LORE IS GOOD OR NOT
 
         if self.lore is None:
             self.lore = self.generate_lore()
-
-        user_happy = False
+        else:
+            user_happy = Ture
+        
         while not user_happy:
-            pass
+            
+            self.generate_lore
+
+
 
         pass
