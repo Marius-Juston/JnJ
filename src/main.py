05790fda55bc9bd67e01df216b4aa0c27b5328a0
Marius-Juston <Marius.juston@hotmail.fr>
2024-08-03 14:41:47 -0400

    added very simple discord example

--- src/main.py
+++ src/main.py
@@ -1,6 +1,22 @@
+import discord
 from dotenv import dotenv_values
+
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print(f'Logged on as {self.user}!')
+
+    async def on_message(self, message):
+        print(f'Message from {message.author}: {message.content}')
+
 
 if __name__ == '__main__':
     config = dotenv_values(".env")
 
     print(config)
+
+    intents = discord.Intents.default()
+    intents.message_content = True
+
+    client = MyClient(intents=intents)
+    client.run(config['BOT_TOKEN'])
