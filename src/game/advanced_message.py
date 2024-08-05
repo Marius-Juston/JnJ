cd5c40f121928d988e3c08c5e2dc622517e4467a
Marius-Juston <Marius.juston@hotmail.fr>
2024-08-05 00:53:59 -0400

    added ability to automatically remove all buttons after selection

--- src/game/advanced_message.py
+++ src/game/advanced_message.py
@@ -18,38 +18,6 @@ class MessageHandler:
                 self.messages.remove(message)
 
 
-class AdvancedMessage(ABC):
-
-    def __init__(self, embed=None, content=None, emojis=None, author_id=None):
-        self.emojis = emojis
-        self.embed = embed
-        self.content = content
-
-        # Check if any info is expected to be sent for message
-        if not (self.content or self.embed):
-            self.content = '_ _'  # create message with empty text
-
-        self.sent_message = None
-
-        if self.emojis:
-            self.view = EmojiSelection(emojis, author_id=author_id, callback_handler=self.callback)
-        else:
-            self.view = None
-
-    @abstractmethod
-    async def callback(self, interaction, emoji):
-        raise NotImplementedError()
-
-    def is_sent(self):
-        return self.sent_message is not None
-
-    async def send(self, location: Union[InteractionResponse, TextChannel]):
-        if isinstance(location, InteractionResponse):
-            self.sent_message = await location.send_message(content=self.content, embed=self.embed, view=self.view)
-        else:
-            self.sent_message = await location.send(content=self.content, embed=self.embed, view=self.view)
-
-
 class EmojiSelection(View):
     def __init__(self, emojis: List[str], author_id: Optional[int] = None,
                  callback_handler: Optional[Callable[[str, Union[InteractionResponse, TextChannel], ], Any]] = None):
@@ -69,3 +37,45 @@ class EmojiSelection(View):
     async def interaction_check(self, interaction: Interaction, /) -> bool:
         print(interaction.user.id, self.author_id)
         return not self.author_id or interaction.user.id == self.author_id
+
+
+class AdvancedMessage(ABC):
+    view: Optional[EmojiSelection]
+
+    def __init__(self, embed=None, content=None, emojis=None, author_id=None, remove_buttons_on_finished=True):
+        self.emojis = emojis
+        self.embed = embed
+        self.content = content
+        self.remove_buttons_on_finished = remove_buttons_on_finished
+
+        self.is_finished = False
+
+        # Check if any info is expected to be sent for message
+        if not (self.content or self.embed):
+            self.content = '_ _'  # create message with empty text
+
+        self.sent_message = None
+
+        if self.emojis:
+            self.view = EmojiSelection(emojis, author_id=author_id, callback_handler=self._callback)
+        else:
+            self.view = None
+
+    async def _callback(self, interaction: Interaction, emoji: str):
+        self.is_finished = await self.callback(interaction, emoji)
+
+        if self.is_finished and self.remove_buttons_on_finished:
+            await interaction.message.edit(view=None)
+
+    @abstractmethod
+    async def callback(self, interaction: Interaction, emoji: str) -> bool:
+        raise NotImplementedError()
+
+    def is_sent(self):
+        return self.sent_message is not None
+
+    async def send(self, location: Union[InteractionResponse, TextChannel]):
+        if isinstance(location, InteractionResponse):
+            self.sent_message = await location.send_message(content=self.content, embed=self.embed, view=self.view)
+        else:
+            self.sent_message = await location.send(content=self.content, embed=self.embed, view=self.view)
