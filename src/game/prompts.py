c78997df0cb9ab6e71d5314a07c1ae7ed13be6de
Marius-Juston <Marius.juston@hotmail.fr>
2024-08-05 00:56:17 -0400

    created example usage of how to create a prompt using the advanced message framework

---
+++ src/game/prompts.py
@@ -0,0 +1,37 @@
+from game.advanced_message import AdvancedMessage
+
+
+# Example class for user prompting
+class UserPrompt(AdvancedMessage):
+    def __init__(self, author_id=None):
+        """
+        Example:
+            advanced = UserPrompt(author_id=interaction.user.id)
+
+            # await advanced.send(interaction.channel)
+            await advanced.send(interaction.response)
+
+            print("Waiting")
+            await advanced.wait_till_finished()
+            print("Finished wating")
+
+        :param author_id:
+        """
+        super().__init__(content='Are you happy?', emojis=['✅', '❌'], author_id=author_id)
+
+        self.made_choice = False
+        self.choice = None
+
+    def finished(self):
+        return self.made_choice
+
+    async def callback(self, interaction, emoji):
+        print(interaction, emoji)
+
+        self.made_choice = True
+
+        self.choice = '✅' == emoji
+
+        await interaction.response.send_message('I selected ' + emoji)
+
+        return True
