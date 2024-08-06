7c905b67052dbf5964a2fbd0eb5842c31eae2157
ArmandJ1 <aretherebouts@proton.me>
2024-08-06 11:26:06 -0400

    changed flow of embed creation

--- src/game/adventure.py
+++ src/game/adventure.py
@@ -1,5 +1,6 @@
 import discord
-from prompts import UserPrompt
+from game.prompts import UserPrompt
+import asyncio
 
 class Adventure:
     def __init__(self, theme, lore):
@@ -7,44 +8,41 @@ class Adventure:
 
         self.theme = theme
         self.lore = lore
+        print("strarting adventure")
 
     def generate_lore(self):
         # TODO : FUTURE WILL BE DONE USING LLM
         print("generate_lore ran")
+        
         return "The pie is a lie"
 
     async def process_lore(self, interaction: discord.Interaction):
-        # TODO : GENERATE LORE HERE IF IT DOES NOT EXIST AND ASK USER IF THE CURRENT LORE IS GOOD OR NOT
-        user_happy = False
+        print("process_lore running")
+
         if self.lore is None:
-            print("No lore input")
-            user_happy = False
+            print("no lore input")
+            await interaction.response.defer(thinking= True)
+            self.embed_lore = self.generate_lore()
+            await asyncio.sleep(1) # TODO: remove when add generate_lore
+            msg = await interaction.original_response()
+
+            embed = self.adventure_announcement()
+            await msg.edit(embed=embed)
+
+            interaction.response.send_message(content= "Are you hapy with the current generated lore?")
+
         else:
-            user_happy = True
-
-        if user_happy is False:
-            self.lore = self.generate_lore()
-            print("New lore generated")
-            self.adventure_announcement()
-            # TODO : prompt user if they like the lore
-            self.user_choice = player_prompt()
-            if user_choice == 0:
-                self.lore = None
-                self.process_lore(interaction)
-            if user_choice == 1:
-                user_happy = True
-                return self.lore
-        else:
-            self.adventure_announcement()
-
-        return self.lore
-
-    async def adventure_announcement(self):
-        # TODO :  add the player list
-        embed = discord.Embed(title="New Adventure", description="This is an embed that will show how to build an embed and the different components", color=0x109319)
-        embed.add_field(name="Theme", value=self.theme, inline=False)
-        embed.add_field(name="Lore", value=self.lore, inline=False)
-        embed.add_field(name="Player list", value=[], inline=False)
-        embed.set_footer(text="Please use /Join_Adventure to join current adventure and type /begin_adventure when all players are ready ready")
+            print("there is lore")
+            self.embed_lore = self.lore
+            adventure_message = self.adventure_announcement()
+            await interaction.response.send_message(embed = adventure_message)
         
-        pass
+    def adventure_announcement(self):
+        print("adventure_announcement running")
+        embed_title = "New Adventure"
+        # TODO :  add the player list
+        embed = discord.Embed(title=embed_title, description="This is an embed that will show how to build an embed and the different components", color=0x109319)
+        embed.add_field(name="Theme", value=self.theme, inline=False)
+        embed.add_field(name="Lore", value=self.embed_lore, inline=False)
+        embed.set_footer(text="Please use /Join_Adventure to join current adventure and type /begin_adventure when all players are ready ready")
+        return embed
