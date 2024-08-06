1a7cff7f77c5db0917d6a95c91b81758b6b66310
ArmandJ1 <aretherebouts@proton.me>
2024-08-05 21:05:34 -0400

    testing advanced messages

--- src/game/adventure.py
+++ src/game/adventure.py
@@ -11,13 +11,13 @@ class Adventure:
 
     def generate_lore(self):
         # TODO : FUTURE WILL BE DONE USING LLM
-
+        print("generate_lore ran")
         return "The pie is a lie"
 
     async def process_lore(self, interaction: discord.Interaction):
         # TODO : GENERATE LORE HERE IF IT DOES NOT EXIST AND ASK USER IF THE CURRENT LORE IS GOOD OR NOT
         user_happy = False
-        if self.lore is None:  # should I do Some instead of None
+        if self.lore is None:
             print("No lore input")
             user_happy = False
         else:
@@ -26,6 +26,7 @@ class Adventure:
         if user_happy is False:
             self.lore = self.generate_lore()
             print("New lore generated")
+            self.adventure_announcement()
             # TODO : prompt user if they like the lore
             self.user_choice = player_prompt()
             if user_choice is 0:
@@ -41,13 +42,10 @@ class Adventure:
 
     async def adventure_announcement(self, ctx):
         # TODO :  add the player list
-        embed = discord.Embed(title="New Adventure",
-                              description="This is an embed that will show how to build an embed and the different components",
-                              color=0x109319)
+        embed = discord.Embed(title="New Adventure", description="This is an embed that will show how to build an embed and the different components", color=0x109319)
         embed.add_field(name="Theme", value=self.theme, inline=False)
         embed.add_field(name="Lore", value=self.lore, inline=False)
         embed.add_field(name="Player list", value=[], inline=False)
-        embed.set_footer(
-            text="Please use /Join_Adventure to join current adventure and type /begin adventure when ready")
-
+        embed.set_footer(text="Please use /Join_Adventure to join current adventure and type /begin_adventure when all players are ready ready")
+        AdvancedMessage()
         pass
