fbf4ace19ff1e6277564fdeb807682a70fc77ec9
Marius-Juston <Marius.juston@hotmail.fr>
2024-08-09 23:05:02 -0400

    added default value to class and character names

--- src/game/player.py
+++ src/game/player.py
@@ -26,13 +26,10 @@ class Player:
         self.max_hp = 100
         self.current_hp = self.max_hp
 
-        self.character_name = None
-        self.class_name = None
-        self.race_name = None
+        self.character_name = discord_user.display_name
+        self.class_name = "Fighter"
+        self.race_name = "Human"
         self.background_lore = None
-
-    def setup_player(self, name, class_name, race_name):
-        pass
 
     async def setup_stats(self, interaction: discord.Interaction):
         print(self.stats)
