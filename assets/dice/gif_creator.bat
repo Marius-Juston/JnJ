ea99d7c64f690e4b7b9c91b14c91b0cbe811a10e
Marius Juston <Marius.juston@hotmail.fr>
2024-08-15 02:20:21 -0500

    added more optimized GIF creation with static background assumption

--- assets/dice/gif_creator.bat
+++ assets/dice/gif_creator.bat
@@ -24,14 +24,18 @@ for /l %%i in (1,1,20) do (
         REM Change directory to temp to process the images
         cd temp
 
-        REM Generate the video from the image sequence
+        REM Generate the video from the image sequence with optimized settings
         ffmpeg -y -framerate 60 -i %%04d.png -c:v libx264 -preset slow -crf 18 -pix_fmt yuv420p output%%i.mp4
 
-        REM Run the first ffmpeg command to generate the palette
-        ffmpeg -y -i output%%i.mp4 -vf "palettegen" palette.png
+        REM Apply a frame difference filter to detect only changing regions (optimize for static backgrounds)
+        ffmpeg -y -i output%%i.mp4 -vf "minterpolate,fps=30,mpdecimate" output_static_optimized%%i.mp4
+
+        REM Generate the palette for the GIF from the optimized video
+        ffmpeg -y -i output_static_optimized%%i.mp4 -vf "palettegen=max_colors=256" palette.png
         
-        REM Run the second ffmpeg command to create the GIF with the palette
-        ffmpeg -y -i output%%i.mp4 -i palette.png -filter_complex "fps=30,scale=640:-1:flags=lanczos,paletteuse=dither=sierra2_4a" ../gifs/%%i.gif
+        REM Create the GIF using the palette, optimized for static backgrounds and 30 FPS
+        ffmpeg -y -i output_static_optimized%%i.mp4 -i palette.png -filter_complex "fps=30,scale=640:-1:flags=lanczos,paletteuse=dither=sierra2_4a" -gifflags +transdiff ../gifs/%%i.gif
+
         
         REM Return to the original directory
         cd ..
