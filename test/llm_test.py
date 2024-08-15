c91f12b7c3edf3d47d92feabd44485e6078afda5
Marius Juston <Marius.juston@hotmail.fr>
2024-08-14 22:50:25 -0500

    create test script for LLM system prompting

---
+++ test/llm_test.py
@@ -0,0 +1,77 @@
+from langchain_ollama import ChatOllama
+from typing import List
+
+from langchain_ollama import ChatOllama
+from typing_extensions import TypedDict
+import json
+
+
+def player_info(class_name: str, race_name: str, background_lore: str) -> bool:
+    """This function is meant to save the player information for the Dungeon and Dragon game.
+
+    Args:
+        class_name: (str) The DnD class of the character
+        race_name: () The DnD race of the character.
+        background_lore: (str) The detailed and comprehensive background lore of the character..
+    """
+    return True
+
+
+def test_player_prompt():
+    tools = [player_info]
+
+    tools_dict = {func.__name__: func for func in tools}
+
+    llm = ChatOllama(
+        model="mistral-nemo",
+        temperature=0,
+    ).bind_tools(tools)
+
+    result = llm.invoke(
+        "Could you validate user 123? They previously lived at "
+        "123 Fake St in Boston MA and 234 Pretend Boulevard in "
+        "Houston TX."
+    )
+
+    for tool in result.tool_calls:
+        result = tools_dict[tool['name']](**tool['args'])
+        print(result)
+
+
+def test_lore_prompt():
+    with open("config/llm_config.json", 'r') as f:
+        config = json.load(f)
+
+    with open(f"config/{config['lore']}", 'r') as f:
+        system_prompt = f.read()
+
+    print(system_prompt)
+
+    llm = ChatOllama(
+        model="mistral-nemo"
+    )
+
+    messages = [
+        (
+            "system", system_prompt
+        ),
+        ("human", "Sci-fi Miniaturization War")
+    ]
+
+    chunks = ''
+
+    for chunk in llm.stream(messages):
+        message = chunk.content
+
+        if len(chunks) + len(message) > config['max_message_length']:
+            print(len(chunks))
+            chunks = ''
+
+        chunks += message
+
+    if chunks:
+        print(len(chunks))
+
+
+if __name__ == '__main__':
+    test_lore_prompt()
